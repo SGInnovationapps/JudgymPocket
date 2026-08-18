8e7217c8ce0516f594ccba168d7962b2ad7d61aa
SGInnovationapps <gymspiral@gmail.com>
2026-08-18 13:15:10 +0900

    Bump cache version to judgympocket-v8
    Updated cache version to v8 for service worker.

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* JudgymPocket - service worker
    アプリ本体をキャッシュして、オフラインでも起動できるようにする。
    ファイルを更新したら CACHE のバージョン番号を上げること。 */
-const CACHE = 'judgympocket-v5';
+const CACHE = 'judgympocket-v8';
 const ASSETS = [
   './',
   './index.html',
